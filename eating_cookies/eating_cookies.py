1342fd6149524b01848650a3629176bfcc2d5cd8
Robert Keffury <bobby.keffury@me.com>
2020-08-14 15:02:21 -0700

    Completed Eating Cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,20 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(n, cache = {}):
 
-    pass
+    if n == 0:
+        return 1
+
+    if n < 0:
+        return 0
+
+    if n in cache.keys():
+        return cache[n]
+
+    value = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    cache[n] = value
+    return value
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
